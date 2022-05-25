a9572f7d2a0b4269c51f060b2b9973506095147c
lyannishere <105715144+lyannishere@users.noreply.github.com>
2022-05-25 16:28:13 +0800

    Update my-first-web-app.py

--- my-first-web-app.py
+++ my-first-web-app.py
@@ -27,7 +27,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
+iris = pd.read_csv('https://raw.githubusercontent.com/lyannishere/AA-Coursework-final/main/IRIS.csv')
 X = iris.data
 Y = iris.target
 
