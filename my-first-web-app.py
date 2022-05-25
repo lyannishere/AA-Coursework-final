71049784f1321a059a0130f30158cc05355dad39
lyannishere <105715144+lyannishere@users.noreply.github.com>
2022-05-25 23:06:24 +0800

    Update my-first-web-app.py

--- my-first-web-app.py
+++ my-first-web-app.py
@@ -39,7 +39,8 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(['Iris-setosa','Iris-versicolor','Iris-virginica'])
+# st.write(['Iris-setosa','Iris-versicolor','Iris-virginica'])
+st.write(pd.Series(['setosa','versicolor','virginica']))
 
 st.subheader('Prediction')
 #st.write(iris.target_names[prediction])
